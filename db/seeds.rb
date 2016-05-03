bdf3f2633f30d9d96be7434017fecfb0fb00ce15
Duke <dukekhaos@gmail.com>
2016-05-02 23:59:35 -0300

    add data to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+PLAYLISTS = [
+  "youtube.com/playlist?list=PLF2J-8QoLzYGj08pCRhjb_1hTl8R1LJjJ",
+  "youtube.com/playlist?list=PLWVIs26n5ziC8QgkkDDC4zKHIJ5F_hMMz",
+  "youtube.com/playlist?list=PLD6DA5E296071FDB3",
+  "youtube.com/playlist?list=PLSG_IHPERQrX_wS-2tJQq--z9LJ8IXMhH",
+  "youtube.com/playlist?list=PLhgGa5awm26NjVjniyuIG19xwUEe48Vyt",
+  "youtube.com/playlist?list=PL-5888xShjYrLX_N42w4EHq-A4_p1YZeP"
+]
+
+User.all.map(&:destroy)
+Course.all.map(&:destroy)
+
+user = User.create! email: 'user@user.com', password: 'abc12345'
+user.confirm!
+
+PLAYLISTS.each do |url|
+  Course.import_from_youtube url, user
+end
