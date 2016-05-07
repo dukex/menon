4d38d21a39e77da4956b8c4a0a82cacf465b3307
Duke <dukekhaos@gmail.com>
2016-05-07 10:47:17 -0300

    fix smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'menon.herokuapp.com', port: 80 }
 
-  config.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
