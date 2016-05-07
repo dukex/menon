0a97e31d8b47a605601c4ef256988fe473674ab8
Duke <dukekhaos@gmail.com>
2016-05-06 23:45:39 -0300

    config smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'menon.herokuapp.com', port: 80 }
+
+  config.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
