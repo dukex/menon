013ce9984672bfaa591df176937c0ff66d5f800f
Duke <dukekhaos@gmail.com>
2016-05-11 07:35:44 -0300

    improve lesson page

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,16 +1,30 @@
 module LessonsHelper
-  def lesson_status_class_for(user, lesson)
+  def lesson_status_class_for(user, lesson, current_lesson)
     classes = ['lesson']
     classes.push 'finished' if lesson.status_for(user).finished
-    classes.push 'current' if lesson.id == @lesson.id
+    classes.push 'current' if lesson.id == current_lesson.id
     classes.join(' ')
   end
 
+  def lesson_icon_for(user, lesson, current_lesson)
+    i = 'circle'
+    i = 'check-circle-o' if lesson.status_for(user).finished
+    i = 'dot-circle-o' if lesson.id == current_lesson.id
+    i
+  end
+
+  # TODO: test
   def has_previous_lesson(course, lesson)
     course.lessons.index(lesson) != 0
   end
 
+  # TODO: test
   def has_next_lesson(course, lesson)
     course.lessons.index(lesson) != course.lessons.count - 1
+  end
+
+  # TODO: test
+  def hyperlink(text)
+    return text.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a target="blank" href=\'\1\'>\1</a>').html_safe
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -10,30 +10,28 @@ class Course < ActiveRecord::Base
     course = self.new owner: user
     playlist = Yt::Playlist.new url: url
 
-    begin
-      course.assign_attributes name: playlist.title,
-        description: playlist.description,
-        thumbnail_url: playlist.thumbnail_url,
-        source_url: url
+    course.assign_attributes name: playlist.title,
+      description: playlist.description,
+      thumbnail_url: playlist.thumbnail_url,
+      source_url: url
 
-      lessons = playlist.playlist_items.map do |item|
-        YoutubeLesson.new name: item.title,
-          description: item.description,
-          thumbnail_url: item.thumbnail_url(:high),
-          published_at: item.published_at,
-          provider_id: item.video.id,
-          position: item.position,
-          duration: item.video.duration,
-          course_id: course.id
-      end
-
-      course.lessons = lessons
-      course.save
-    rescue Yt::Error => error
-      course.errors.add(:base, error.kind["message"])
-    ensure
-      return course
+    lessons = playlist.playlist_items.map do |item|
+      YoutubeLesson.new name: item.title,
+        description: item.description,
+        thumbnail_url: item.thumbnail_url(:high),
+        published_at: item.published_at,
+        provider_id: item.video.id,
+        position: item.position,
+        duration: item.video.duration,
+        course_id: course.id
     end
+
+    course.lessons = lessons
+    course.save
+  rescue Yt::Error => error
+    course.errors.add(:base, error.kind["message"])
+  ensure
+    return course
   end
 
   def progress_for(user)
