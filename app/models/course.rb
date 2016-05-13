0fdd88c259a0bb13cc11b7e7bde3920ae8df3671
Duke <dukekhaos@gmail.com>
2016-05-13 08:10:17 -0300

    improve youtube lesson thumbnail

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,18 +7,18 @@ class Course < ActiveRecord::Base
   validates :name, presence: true
 
   def self.import_from_youtube(url, user)
+    # TODO: change user params just to id
     course = self.new owner: user
     playlist = Yt::Playlist.new url: url
 
     course.assign_attributes name: playlist.title,
       description: playlist.description,
-      thumbnail_url: playlist.thumbnail_url,
       source_url: url
 
     lessons = playlist.playlist_items.map do |item|
       YoutubeLesson.new name: item.title,
         description: item.description,
-        thumbnail_url: item.thumbnail_url(:high),
+        thumbnail_url:  "http://img.youtube.com/vi/#{item.video.id}/hqdefault.jpg",
         published_at: item.published_at,
         provider_id: item.video.id,
         position: item.position,
@@ -32,6 +32,10 @@ class Course < ActiveRecord::Base
     course.errors.add(:base, error.kind["message"])
   ensure
     return course
+  end
+
+  def thumbnail_url
+    lessons.first.thumbnail_url
   end
 
   def progress_for(user)
