658f64285f3b3b5417ba6c8d4b9b2846cbb0a15f
Duke <dukekhaos@gmail.com>
2016-05-20 07:45:31 -0300

    improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
-PLAYLISTS = [
-  "youtube.com/playlist?list=PLF2J-8QoLzYGj08pCRhjb_1hTl8R1LJjJ",
-  "youtube.com/playlist?list=PLWVIs26n5ziC8QgkkDDC4zKHIJ5F_hMMz",
-  "youtube.com/playlist?list=PLD6DA5E296071FDB3",
-  "youtube.com/playlist?list=PLSG_IHPERQrX_wS-2tJQq--z9LJ8IXMhH",
-  "youtube.com/playlist?list=PLhgGa5awm26NjVjniyuIG19xwUEe48Vyt",
-  "youtube.com/playlist?list=PL-5888xShjYrLX_N42w4EHq-A4_p1YZeP"
-]
-
-user = User.create! email: 'user@user.com', password: 'abc12345', confirmed_at: Time.now
-
-PLAYLISTS.each do |url|
-  Course.import_from_youtube url, user
-end
+Course.create!([
+  {name: "Literature", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtOeEc9ME62zTfqc0h6Pe8vb"},
+  {name: "Ecology", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtNdTKZkV_GiIYXpV9w4WxbX"},
+  {name: "US History", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtMwmepBjTSG593eG7ObzO7s"},
+  {name: "Chemistry", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtPHzzYuWy6fYEaX9mQQ8oGr"},
+  {name: "Biology", source_url: "https://www.youtube.com/playlist?list=PL3EED4C1D684D3ADF"},
+  {name: "World History", source_url: "https://www.youtube.com/playlist?list=PLBDA2E52FB1EF80C9"},
+  {name: "Psychology", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtOPRKzVLY0jJY-uHOH9KVU6"},
+  {name: "Big History", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtMczXZUmjb3mZSU1Roxnrey"},
+  {name: "U.S. Government and Politics", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtOfse2ncvffeelTrqvhrz8H"},
+  {name: "Astronomy", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtPAJr1ysd5yGIyiSFuh0mIL"},
+  {name: "Anatomy & Physiology", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtOAKed_MxxWBNaPno5h3Zs8"},
+  {name: "Intellectual Property", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtMwV2btpcij8S3YohW9gUGN"},
+  {name: "World History 2", source_url: "https://www.youtube.com/playlist?list=PL8dPuuaLjXtNjasccl-WajpONGX3zoY4M"},
+  {name: "German for BEGINNERS A1 - FREE Course Lessons 1-100", source_url: "https://www.youtube.com/playlist?list=PLYzp2xhTw9W1Z9RvnCoveC0W7pkwUMHXy"},
+  {name: "Lecciones de Economía con Jesús Huerta de Soto", source_url: "https://www.youtube.com/playlist?list=PLSKbCQY095R4jp6PoFulCDLYIzzdbD1DI"}
+])
