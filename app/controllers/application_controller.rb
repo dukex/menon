2fc71c89bde2ab1331521413dd827d5c8d4c8930
Duke <dukekhaos@gmail.com>
2016-06-04 09:39:07 -0300

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   respond_to :html
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale_from_cookie || I18n.default_locale
+  end
+
+  def extract_locale_from_cookie
+    cookies[:locale]
+  end
 end
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -31,6 +31,6 @@ module LessonsHelper
 
   # TODO: test
   def hyperlink(text)
-    return text.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a target="blank" href=\'\1\'>\1</a>').html_safe
+    return text && text.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a target="blank" href=\'\1\'>\1</a>').html_safe
   end
 end
