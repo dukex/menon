64e57499b7ba0d5279d594a80d09a8bacac14405
Emerson Almeida <emersonalmeidax@gmail.com>
2022-05-10 02:51:18 -0300

    add lessons and youtube lessons

---
+++ app/dashboards/courses/youtube_lesson_dashboard.rb
@@ -0,0 +1,4 @@
+require 'administrate/base_dashboard'
+
+class Courses::YoutubeLessonDashboard < Courses::LessonDashboard
+end
---
+++ app/models/courses/youtube_lesson.rb
@@ -0,0 +1,4 @@
+module Courses
+  class YoutubeLesson < Lesson
+  end
+end
