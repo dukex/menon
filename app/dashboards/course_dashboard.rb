7ca8cc6bf37b8828c80465db52b9a1e6489f894a
Emerson Almeida <emersonalmeidax@gmail.com>
2022-05-10 02:56:41 -0300

    improve course dashboard

--- app/dashboards/course_dashboard.rb
+++ app/dashboards/course_dashboard.rb
@@ -10,16 +10,19 @@ class CourseDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    id: Field::String,
+    slug: Field::String,
     name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    status: Field::Select.with_options(collection: Course.statuses.keys),
+    language: Field::String,
     description: Field::Text,
     source_url: Field::String,
     thumbnail_url: Field::String,
-    slug: Field::String,
-    id: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
     owner_id: Field::String,
-    language: Field::String
+    lessons: Field::HasMany.with_options(limit: 1000, direction: :desc, sort_by: :created_at,
+                                         class_name: 'Courses::Lesson')
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -28,39 +31,19 @@ class CourseDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    slug
     name
-    created_at
-    updated_at
-    description
+    status
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = %i[
-    name
-    created_at
-    updated_at
-    description
-    source_url
-    thumbnail_url
-    slug
-    id
-    owner_id
-    language
-  ].freeze
+  SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = %i[
-    name
-    description
-    source_url
-    thumbnail_url
-    slug
-    owner_id
-    language
-  ].freeze
+  FORM_ATTRIBUTES = ATTRIBUTE_TYPES.keys - %i[created_at updated_at owner lessons]
 
   # COLLECTION_FILTERS
   # a hash that defines filters that can be used while searching via the search
@@ -72,12 +55,17 @@ class CourseDashboard < Administrate::BaseDashboard
   #   COLLECTION_FILTERS = {
   #     open: ->(resources) { resources.where(open: true) }
   #   }.freeze
-  COLLECTION_FILTERS = {}.freeze
-
+  COLLECTION_FILTERS = {
+    importing: ->(resources) { resources.where(status: :importing) }
+  }
   # Overwrite this method to customize how courses are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(course)
-  #   "Course ##{course.id}"
-  # end
+  def display_resource(course)
+    course.name.to_s
+  end
+
+  def import_page_attributes
+    [:status]
+  end
 end
