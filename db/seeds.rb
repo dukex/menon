f94dff9f17a22d760af3e3942f36d48b17bb9f14
Emerson Almeida <emersonalmeidax@gmail.com>
2022-05-12 00:18:10 -0300

    create a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+[
+  ['https://www.youtube.com/playlist?list=PLUl4u3cNGP63EdVPNLG3ToM6LaEUuStEY', '6006'],
+  ['https://www.youtube.com/playlist?list=PLUl4u3cNGP63WbdFxL8giv4yhgdMGaZNA', '60001'],
+  ['https://www.youtube.com/playlist?list=PLUl4u3cNGP619EG1wp0kT-7rDE_Az5TNd', '60002'],
+  ['https://www.youtube.com/playlist?list=PL9B24A6A9D5754E70', '601SC'],
+  ['https://www.youtube.com/playlist?list=PLUl4u3cNGP6317WaSNfmCvGym2ucw3oGp', '6046J'],
+  ['https://www.youtube.com/playlist?list=PLUl4u3cNGP63UUkfL0onkxF6MYgVa04Fn', '15S12']
+].each do |playlist|
+  Course.create! name: playlist.last, source_url: playlist.first
+end
