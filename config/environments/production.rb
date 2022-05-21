306d8db0de177fcf0087ddf86845d0a0beac3612
Emerson Almeida <emersonalmeidax@gmail.com>
2022-05-21 12:21:22 -0300

    force ssl in asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = ENV.fetch('ASSET_HOST')
+  config.asset_host = ->(_source) { "https://#{ENV.fetch('ASSET_HOST')}" }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
