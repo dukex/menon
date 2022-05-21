e3b2219d704d7e289286ca496d59d2e70b75a7ac
Emerson Almeida <emersonalmeidax@gmail.com>
2022-05-21 20:12:21 -0300

    remove ruby redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_from_www
-
-  def redirect_from_www
-    if request.host.match?(/^www\./)
-      redirect_to url_for(host: request.host.sub('www.', '')), allow_other_host: true,
-                                                               status: :moved_permanently
-    end
-  end
 end
