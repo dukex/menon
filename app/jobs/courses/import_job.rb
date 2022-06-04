f5e282e57cc1206260c74b6a7a43a4f52010383f
Emerson Almeida <emersonalmeidax@gmail.com>
2022-06-04 18:04:21 -0300

    fix typo

--- app/jobs/courses/import_job.rb
+++ app/jobs/courses/import_job.rb
@@ -7,7 +7,7 @@ module Courses
       course.provider.import!
       course.imported!
     rescue Yt::Errors::Forbidden => e
-      @course.retry_soon!
+      course.retry_soon!
       raise e
     rescue StandardError => e
       course.error!
