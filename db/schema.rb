38a50ac6abd59a3e21a2d1368a47f169db2cc6f1
Emerson Almeida <emersonalmeidax@gmail.com>
2022-06-06 01:32:30 -0300

    add course languages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_035309) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_042946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -195,5 +195,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_035309) do
       result.category
      FROM result
     ORDER BY result.section;
+  SQL
+  create_view "courses_languages", materialized: true, sql_definition: <<-SQL
+      SELECT courses.language,
+      count(*) AS courses
+     FROM courses
+    GROUP BY courses.language;
   SQL
 end
