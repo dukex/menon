e33aad864d0f6db8f9acfc99bdbd3037469b4539
Emerson Almeida <emersonalmeidax@gmail.com>
2022-06-18 18:34:47 -0300

    increase the item by category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_213028) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_18_213359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -180,7 +180,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_213028) do
               top_categories.creator_name,
               top_categories.creator_url
              FROM top_categories
-            WHERE (top_categories.rank <= 2)
+            WHERE (top_categories.rank <= 4)
           ), result AS (
           ( SELECT featured.section,
               featured.id,
