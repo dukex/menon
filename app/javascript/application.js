efe968d612bbf0f2c5cb32dd90e0b8e73e026988
surykt <cardosocarlosalan@gmail.com>
2022-07-04 14:46:26 -0300

    add toogle to menu header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+const menu = document.querySelector("#menu");
+
+menu.classList.remove("hidden");
+
+menu.addEventListener("click", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("bg-brand-dark");
+  header.classList.toggle("text-white");
+  document.querySelector("nav").classList.toggle("hidden");
+});
