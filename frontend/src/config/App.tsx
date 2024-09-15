92908a8f4b0b1cfb28c5366902a27edc0276a967
Emerson Almeida <emersonalmeidax@gmail.com>
2024-09-15 12:06:42 -0300

    fix config

--- frontend/src/config/App.tsx
+++ frontend/src/config/App.tsx
@@ -1,11 +1,10 @@
-
 export const App = {
-    apiURL: 'http://localhost:8788',
-    siteName: 'Menon',
-    title: 'Menon',
-    description:
-      'Turn any YouTube playlist into an organized, interactive course with our platform. Enhance your learning experience with progress tracking, quizzes, and more. Get started now!',
-    locale: 'en',
-  };
-  
+  apiURL: process.env.API_URL,
+  siteName: "Menon",
+  title: "Menon",
+  description:
+    "Turn any YouTube playlist into an organized, interactive course with our platform. Enhance your learning experience with progress tracking, quizzes, and more. Get started now!",
+  locale: "en",
+};
+
 export default App;
