a5b85d21696a1cc3d69eafaeca9d389383579460
Emerson Almeida <emersonalmeidax@gmail.com>
2024-09-20 01:00:32 -0300

    improve course card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -31,30 +31,32 @@ export default async function Page() {
         <h2 className="text-3xl font-bold mb-6 text-gray-800">
           Available Courses
         </h2>
-        <div className="flex gap-4 flex-wrap">
+        <div className="flex flex-wrap">
           {courses.map((course) => (
             <div
               key={course.id}
-              className="bg-white rounded-lg shadow-md overflow-hidden w-1/2 md:w-1/3 lg:w-1/5 flex flex-col"
+              className="w-1/2 md:w-1/3 lg:w-1/5 flex flex-col p-2"
             >
-              <Image
-                src={course.thumbnail_url}
-                alt={course.name}
-                width={400}
-                height={200}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-6 flex-1">
-                <h2 className="text-xl font-semibold mb-2">{course.name}</h2>
-                <p className="text-gray-600 mb-4">{course.description}</p>
-              </div>
-              <div className="p-6">
-                <Link
-                  href={`/${course.slug}`}
-                  className="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-                >
-                  View Course
-                </Link>
+              <div className="bg-white rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src={course.thumbnail_url}
+                  alt={course.name}
+                  width={400}
+                  height={200}
+                  className="w-full h-48 object-cover"
+                />
+                <div className="p-6 flex-1">
+                  <h2 className="text-xl font-semibold mb-2">{course.name}</h2>
+                  <p className="text-gray-600 mb-4">{course.description}</p>
+                </div>
+                <div className="p-6">
+                  <Link
+                    href={`/${course.slug}`}
+                    className="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+                  >
+                    View Course
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
