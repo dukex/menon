a331b89a87231f987128eb80e6c90b91e3b9dc3c
Emerson Almeida <emersonalmeidax@gmail.com>
2024-09-29 15:13:55 -0300

    fix getCoursesForUser

--- api/src/repositories/courses/getCoursesForUser.ts
+++ api/src/repositories/courses/getCoursesForUser.ts
@@ -3,10 +3,17 @@ import { Course, IDatabase } from "../../types";
 export async function getCoursesForUser(userId: string, database: IDatabase) {
   const stmt = database
     .prepare(
-      "SELECT DISTINCT c.*, ROUND(SUM(ls.finished + 0.0)/COUNT(cl.id), 2) as progress " +
+      "SELECT ROUND(SUM(ls.finished + 0.0)/COUNT(cl.id), 2) as progress, c.* " +
         "FROM courses c " +
-        "JOIN courses_lessons cl ON c.id = cl.course_id " +
-        "LEFT JOIN lesson_statues ls ON cl.lesson_id = ls.lesson_id AND ls.user_id = ?1"
+        "LEFT JOIN courses_lessons cl ON cl.course_id = c.id " +
+        "LEFT JOIN lesson_statues ls ON ls.lesson_id = cl.lesson_id AND ls.user_id = ?1 " +
+        "WHERE c.id IN ( " +
+        "    SELECT DISTINCT cl.course_id " +
+        "    FROM lesson_statues ls " +
+        "    JOIN courses_lessons cl ON cl.lesson_id = ls.lesson_id " +
+        "    WHERE ls.user_id = ?1 " +
+        ") " +
+        "GROUP BY c.id"
     )
     .bind(userId);
 
